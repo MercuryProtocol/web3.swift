e3fa39a3d3e610c8cd78e77ceb2d5b18e04679fe
Sameer Khavanekar <sameer@cyberdust.com>
2018-01-19 12:04:58 -0800

    Add example app

---
+++ Example/iOS Example/iOS Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS Example
+//
+//  Created by Sameer Khavanekar on 1/19/18.
+//  Copyright © 2018 Radical App LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
